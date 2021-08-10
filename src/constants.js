8d6886663030b870b0b66a88719d71a708c0aa04
Superstar-IT <freewebdev0213@gmail.com>
2021-08-11 00:24:58 +0800

    update human-job

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,10 @@
-import humanAboutImg from './assets/images/human_about_slider.png';
+import humanAboutImg_1 from './assets/images/human_about_slider_1.png';
+import humanAboutImg_2 from './assets/images/human_about_slider_2.png';
+import humanAboutImg_3 from './assets/images/human_about_slider_3.png';
+import humanAboutImg_4 from './assets/images/human_about_slider_4.png';
+import humanAboutImg_5 from './assets/images/human_about_slider_5.png';
+import humanJobImg_1 from './assets/images/human_job_slider_1.png';
+import humanJobImg_2 from './assets/images/human_job_slider_2.png';
 
 export const PrimaryColor = {
   deepPink: "#781956",
@@ -31,42 +37,46 @@ export const HumanAbout = [
     title: '',
     subTitle: "A new way to work",
     content: "HUMAN Protocol tokenizes and automates the secure publishing of data labeling jobs, and manages the distribution of the work - and its rewards - across distributed workforces. The Protocol offers a new way for humans and machines to collaborate, and functions as a blockchain-based infrastructure for the creation and settlement of secure, globally accessible job markets.",
-    image: humanAboutImg,
+    image: humanAboutImg_1,
   },
   {
     title: '',
-    subTitle: "A new way to work",
-    content: "HUMAN Protocol tokenizes and automates the secure publishing of data labeling jobs, and manages the distribution of the work - and its rewards - across distributed workforces. The Protocol offers a new way for humans and machines to collaborate, and functions as a blockchain-based infrastructure for the creation and settlement of secure, globally accessible job markets.",
-    image: humanAboutImg,
+    subTitle: "An infrastructure for global job markets",
+    content: "HUMAN Protocol operates across multiple chains – with tasks intelligently distributed to optimize their fulfilment – to support global job markets. The Protocol automates all parts of the job cycle - from launch, to quality check, and payment.",
+    image: humanAboutImg_2,
   },
   {
     title: '',
-    subTitle: "A new way to work",
+    subTitle: "Improved human-machine collaboration",
     content: "HUMAN Protocol tokenizes and automates the secure publishing of data labeling jobs, and manages the distribution of the work - and its rewards - across distributed workforces. The Protocol offers a new way for humans and machines to collaborate, and functions as a blockchain-based infrastructure for the creation and settlement of secure, globally accessible job markets.",
-    image: humanAboutImg,
+    image: humanAboutImg_3,
   },
   {
     title: '',
-    subTitle: "A new way to work",
-    content: "HUMAN Protocol tokenizes and automates the secure publishing of data labeling jobs, and manages the distribution of the work - and its rewards - across distributed workforces. The Protocol offers a new way for humans and machines to collaborate, and functions as a blockchain-based infrastructure for the creation and settlement of secure, globally accessible job markets.",
-    image: humanAboutImg,
+    subTitle: "Real-HUMAN connections",
+    content: "Global connections – direct and secure – bring workers closer to the rewards of their work, organizations to workforces, and machines to understanding. Every transfer is a handshake, signifying work requested, work completed, and transactions made. A single job can contain thousands or millions of tasks, completed by Workers distributed across the globe.",
+    image: humanAboutImg_4,
+  },
+  {
+    title: '',
+    subTitle: "Jobs and tasks",
+    content: "A single job can be made up of millions of tasks. In a HUMAN-Protocol-powered application, an individual task may represent any point of human-to-machine interaction: the labeling of data, or sign-off on machine labeling (known as feedback learning). HUMAN Protocol is designed to create a more human world: disintermediated yet connected, with many viewpoints and backgrounds accounted for and represented, in which value produced is rewarded.",
+    image: humanAboutImg_5,
   },
 ];
 
 export const HumanJobs = [
   {
-    title: `Future of <span class='highlight'>Human</span> jobs?`,
-    subTitle: "Subtitle",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    image: "",
+    title: `The Future of <span class='highlight'>HUMAN jobs</span>`,
+    subTitle: "",
+    content: "hCaptcha is only the first application on which HUMAN jobs can be completed. The Protocol supports other applications - such as Intel CVAT and INCEpTION - and the potential for many more; HUMAN Protocol is simply the blockchain-based infrastructure which allows distributed parties to work together trustlessly.",
+    image: humanJobImg_1,
   },
   {
-    title: `Future of <span class='highlight'>Human</span> jobs?`,
-    subTitle: "Subtitle",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    image: "",
+    title: `The Future of <span class='highlight'>HUMAN jobs</span>`,
+    subTitle: "",
+    content: "Many kinds of application can run on the Protocol, delivering the potential for many more jobs for Workers, and new datasets to those who need them.",
+    image: humanJobImg_2,
   },
 ];
 
