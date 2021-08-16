a4f712dc3eb1ed8e82e222457406a2449e1cb572
Superstar-IT <freewebdev0213@gmail.com>
2021-08-16 13:08:58 +0800

    implement aip

--- src/pages/Login/register.jsx
+++ src/pages/Login/register.jsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Link, withRouter } from "react-router-dom";
-import { FormGroup, FormControl, Button } from "react-bootstrap";
+import { Link, Redirect, withRouter } from "react-router-dom";
+import { FormGroup, FormControl, Button, Alert } from "react-bootstrap";
 import * as EmailValidator from 'email-validator';
-import { ErrorMessage } from "../../constants";
+import { ErrorMessage, SignUpOpt } from "../../constants";
 import { Password } from "../../components/inputs/password/password";
 import './login.scss';
-import { register } from "../../service/user.service";
+import { register, linkWallet } from "../../service/user.service";
 import { Routes } from "../../routes";
 
 const RegisterPage = (props) => {
     const { history } = props;
     const dispatch = useDispatch();
+    const [option, setOption] = useState(SignUpOpt.verifyEmail)
+    const [emailVerified, setEmailVerified] = useState(false)
     const [inputs, setInputs] = useState({
         email: '',
         userName: '',
         firstName: '',
         lastName: '',
         password: '',
+        walletAddress: '',
+        verificationToken: '',
     });
+    const [alertMsg, setAlertMsg] = useState('');
+    useEffect(() => {
+        if(history.location.state) 
+            setOption(history.location.state);
+    }, [])
     const [submitted, setSubmitted] = useState(false);
-    const { email, password, userName, firstName, lastName } = inputs;
+    const { email, password, userName, firstName, lastName, walletAddress, verificationToken } = inputs;
     
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -45,52 +54,160 @@ const RegisterPage = (props) => {
         }
     }
 
+    const handleLinkWallet = (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+        if(walletAddress && walletAddress.length === 42) {
+            linkWallet(walletAddress).then(() => {
+                console.log('Success to link Wallet');
+                setAlertMsg('');
+                setOption(SignUpOpt.complete);
+            }).catch((err) => {
+                setAlertMsg(err.message);
+            })
+        }
+    }
+
+    const startQuestionnaire = (e) => {
+        e.preventDefault();
+        setSubmitted(false);
+        history.push({ pathname: Routes.Home.path });
+    }
+
     return (
         <div id='register' className='col-md-4 offset-md-4 d-flex flex-column justify-content-center h-100'>
-            <div className='page-title d-flex justify-content-between mb-4'>
-                <h2>Sign Up</h2>
-                <Link to='/'><i className='material-icons close'>clear</i></Link>
+            { option !== SignUpOpt.complete &&
+            <div className='d-flex justify-content-between'>
+                <div className={ option === SignUpOpt.verifyEmail ? 'opt active' : 'opt' } onClick={(e) => setOption(SignUpOpt.verifyEmail)}>Verify Email</div>
+                <div className={ option === SignUpOpt.register ? 'opt active' : 'opt' } onClick={(e) => setOption(SignUpOpt.register)}>Create Account</div>
+                <div className={ option === SignUpOpt.linkWallet ? 'opt active' : 'opt' } onClick={(e) => setOption(SignUpOpt.linkWallet)}>Link Wallet</div>
             </div>
-            <div>
-                <form name='form' onSubmit={handleSubmit}>
-                    <FormGroup>
-                        <FormControl placeholder='Email' type='email' name='email' value={email} onChange={handleChange}></FormControl>
-                        {submitted && !email &&
-                        <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireEmail}</FormControl.Feedback>
+            }
+            <div className='container'>
+                <div className='page-title d-flex justify-content-between mb-4'>
+                    { option === SignUpOpt.verifyEmail && !emailVerified &&
+                       <h2>Verify email</h2>
+                    }
+                    { option === SignUpOpt.verifyEmail && emailVerified &&
+                       <h2>Email verified</h2>
+                    }
+                    { option === SignUpOpt.register && 
+                       <h2>Create account</h2>
+                    }
+                    { option === SignUpOpt.linkWallet && 
+                       <h2>Link wallet</h2>
+                    }
+                    { option === SignUpOpt.complete && 
+                       <h2>Start earning HMT</h2>
+                    }
+                    <Link to='/'><i className='material-icons close'>clear</i></Link>
+                </div>
+                { alertMsg && alertMsg.length &&
+                <Alert variant="danger" onClose={() => setAlertMsg('')} dismissible>
+                    { option === SignUpOpt.linkWallet && 
+                    <Alert.Heading>Link wallet failed!</Alert.Heading>
+                    }
+                    { option === SignUpOpt.register && 
+                    <Alert.Heading>Register failed!</Alert.Heading>
+                    }
+                    { option === SignUpOpt.verifyEmail && 
+                    <Alert.Heading>Email verification failed!</Alert.Heading>
+                    }
+                    <p>{alertMsg}</p>
+                </Alert>
+                }
+                <div>
+                    <form name='form' onSubmit={handleSubmit}>
+                        { option === SignUpOpt.verifyEmail && 
+                        <>
+                        <FormGroup>
+                            <FormControl placeholder='Token' type='text' name='verificationToken' value={verificationToken} onChange={handleChange}></FormControl>
+                            {submitted && !verificationToken &&
+                            <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireVerificationToken}</FormControl.Feedback>
+                            }
+                        </FormGroup>
+                        <FormGroup className='actions d-flex justify-content-between m-0'>
+                            <Link className='btn' to={{ pathname:Routes.Register.path, state: SignUpOpt.register}}>Back</Link>
+                            <Button className='form-control bg-blue' onClick={handleLinkWallet}>Next</Button>
+                        </FormGroup>
+                        </>
                         }
-                        {submitted && email && !EmailValidator.validate(email) &&
-                        <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.invalidEmail}</FormControl.Feedback>
+                        { option === SignUpOpt.complete && 
+                        <>
+                        <FormGroup>
+                            <p>Complete the questionnaire to receive 1 HMT.</p>
+                        </FormGroup>
+                        <FormGroup className='actions d-flex justify-content-between m-0'>
+                            <Link className='btn' to={Routes.Home.path}>Skip</Link>
+                            <Button className='form-control bg-blue' onClick={startQuestionnaire}>Questionnaire</Button>
+                        </FormGroup>
+                        </>
                         }
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControl placeholder='Username' type='text' name='userName' value={userName} onChange={handleChange}></FormControl>
-                        {submitted && !userName &&
-                        <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireUserName}</FormControl.Feedback>
+                        { option === SignUpOpt.linkWallet &&
+                        <>
+                        <FormGroup>
+                            <p>The wallet address must be KYC-verified. We’ll need this to send you HMT!</p>
+                        </FormGroup>
+                        <FormGroup>
+                            <FormControl placeholder='Wallet address' type='text' name='walletAddress' value={walletAddress} onChange={handleChange}></FormControl>
+                            {submitted && !walletAddress &&
+                            <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireWalletAddress}</FormControl.Feedback>
+                            }
+                            {submitted && walletAddress && walletAddress.length !== 42 &&
+                            <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.invalidWalletAddress}</FormControl.Feedback>
+                            }
+                        </FormGroup>
+                        <p><Link>Click here</Link> to create your crypto wallet</p>
+                        <FormGroup className='actions d-flex justify-content-between m-0'>
+                            <Link className='btn' to={{ pathname:Routes.Register.path, state: SignUpOpt.register}}>Back</Link>
+                            <Button className='form-control bg-blue' onClick={handleLinkWallet}>Next</Button>
+                        </FormGroup>
+                        </> 
                         }
-                    </FormGroup>
-                    <FormGroup className='mb-5'>
-                        <div className='row'>
-                            <div className='col'>
-                                <FormControl placeholder='First Name' type='text' name='firstName' value={firstName} onChange={handleChange}></FormControl>
-                                {submitted && !firstName &&
-                                <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireFirstName}</FormControl.Feedback>
-                                }
+                        { option === SignUpOpt.register && 
+                        <>
+                        <FormGroup>
+                            <FormControl placeholder='Email' type='email' name='email' value={email} onChange={handleChange}></FormControl>
+                            {submitted && !email &&
+                            <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireEmail}</FormControl.Feedback>
+                            }
+                            {submitted && email && !EmailValidator.validate(email) &&
+                            <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.invalidEmail}</FormControl.Feedback>
+                            }
+                        </FormGroup>
+                        <FormGroup>
+                            <FormControl placeholder='Username' type='text' name='userName' value={userName} onChange={handleChange}></FormControl>
+                            {submitted && !userName &&
+                            <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireUserName}</FormControl.Feedback>
+                            }
+                        </FormGroup>
+                        <FormGroup className='mb-5'>
+                            <div className='row'>
+                                <div className='col'>
+                                    <FormControl placeholder='First Name' type='text' name='firstName' value={firstName} onChange={handleChange}></FormControl>
+                                    {submitted && !firstName &&
+                                    <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireFirstName}</FormControl.Feedback>
+                                    }
+                                </div>
+                                <div className='col'>
+                                    <FormControl placeholder='Last Name' type='text' name='lastName' value={lastName} onChange={handleChange}></FormControl>
+                                    {submitted && !firstName &&
+                                    <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireLastName}</FormControl.Feedback>
+                                    }
+                                </div>
                             </div>
-                            <div className='col'>
-                                <FormControl placeholder='Last Name' type='text' name='lastName' value={lastName} onChange={handleChange}></FormControl>
-                                {submitted && !firstName &&
-                                <FormControl.Feedback type='invalid' className='d-block'>{ErrorMessage.requireLastName}</FormControl.Feedback>
-                                }
-                            </div>
-                        </div>
-                    </FormGroup>
-                    <Password onChange={handleChange} value={password} submitted={submitted} className='mb-5'></Password>
-                    <FormGroup>
-                        <Button className='form-control' onClick={handleSubmit}>Next</Button>
-                    </FormGroup>
-                </form>
+                        </FormGroup>
+                        <Password onChange={handleChange} value={password} submitted={submitted} className='mb-5'></Password>
+                        </>
+                        }
+                        {/* <FormGroup className='actions d-flex justify-content-between m-0'>
+                            <Link className='btn' to={Routes.Home.path}>Back</Link>
+                            <Button className='form-control bg-blue'>Next</Button>
+                        </FormGroup> */}
+                    </form>
+                </div>
             </div>
-            </div>
+        </div>
     )
 }
 
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import * as EmailValidator from 'email-validator';
+
 export const authHeader = () => {
   // return authorization header with jwt token
   let user = JSON.parse(localStorage.getItem('user'));
@@ -7,17 +10,42 @@ export const authHeader = () => {
   } else {
       return {};
   }
+
 }
 
 export const register = async (user) => {
-  localStorage.setItem('user', JSON.stringify(user));  
-  return user;
+  if(!user.name) throw new Error(`name required`);
+  if(!user.password) throw new Error(`password required`);
+  if(user.password.length < 5 ) throw new Error(`password is at least 5 length`);
+  if(!user.email) throw new Error(`email required`);
+  if(user.email && !EmailValidator.validate(user.email))
+    throw new Error(`Invalid email`);
+  
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/auth/register`,
+    user,
+  ).then((response) => {
+    if(response) {
+      const { user, tokens } = response.data;
+      localStorage.setItem('token', tokens.access.token);
+      localStorage.setItem('refreshToken', tokens.refresh.token);
+      return user;
+    }
+  })
 }
 
 export const signIn = async ({email, password}) => {
-  localStorage.setItem("token", 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QudXNlckBnbWFpbC5jb20iLCJ1c2VyTmFtZSI6InN0YXJEdXN0IiwiZmlyc3ROYW1lIjoiSmFtZXMiLCJsYXN0TmFtZSI6IkFsZXgiLCJwYXNzd29yZCI6IlFXRTEyMyFAIyJ9.7f7ATho1Qd0e-IO3OhoE-oiCGSPYaKGuKHQgf3bJ7Oo');
-  localStorage.setItem('user', JSON.stringify({ email, password }));
-  return true;
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/auth/login`,
+    { email, password },
+  ).then((response) => {
+    const { user, tokens } = response.data;
+    localStorage.setItem('token', tokens.access.token);
+    localStorage.setItem('refreshToken', tokens.refresh.token);
+    return { user, token: tokens.access.token };
+  }).catch((err) => {
+    throw new Error(err.response.data.message);
+  });
 }
 
 export const update = async (user) => {
@@ -26,6 +54,57 @@ export const update = async (user) => {
 }
 
 export const logOut = async () => {
-  localStorage.removeItem('user');
+  const refreshToken = localStorage.getItem('refreshToken');
+  if(refreshToken && refreshToken.length) {
+    return axios.post(
+      `${process.env.REACT_APP_API_URL}/auth/logout`,
+      { refreshToken },
+    );
+  } else {
+    throw new Error('refreshToken required.')
+  }
+}
+
+export const forgotPassword = async (email) => {
+  if(!email) throw new Error(`email required`);
+  if(email && EmailValidator.validate(email))
+    throw new Error(`Invalid email`);
+
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/auth/forgot-password`,
+    { email },
+  ).catch((err) => {
+    // throw new Error(err.response.data.)
+  })
+} 
+
+export const linkWallet = async (address) => {
+  // throw new Error(`Failed to link wallet`);
   return true;
+}
+
+export const getMyAccount = async (token) => {
+  return {
+    id: '5ebac534954b54139806c112',
+    email: 'fake@example.com',
+    name: 'fake name',
+    role: 'user'
+  }
+}
+
+export const sendVerificationEmail = async (token) => {
+  return axios.post(
+    `${process.env.REACT_APP_API_URL}/auth/send-verification-email`,
+    '',
+    { headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    }
+  ).then((response) => {
+    if(response && response.status === 204)
+      return true;
+    else throw new Error(`Failed to send verification email`);
+  }).catch((err) => {
+    throw new Error(err.response.data.message);
+  })
 }
