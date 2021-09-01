3f327947f47e3612cd244d375e909bce95b28113
Superstar-IT <freewebdev0213@gmail.com>
2021-09-01 17:11:01 +0800

    update

--- src/pages/Login/register.jsx
+++ src/pages/Login/register.jsx
@@ -35,6 +35,7 @@ const RegisterPage = (props) => {
         refCode: '',
     });
     const [submitted, setSubmitted] = useState(false);
+    const [submitable, setSubmitable] = useState(false);
     const [alertMsg, setAlertMsg] = useState('');
     const [captchaPassed, setCaptchaPassed] = useState(false);
     const [hcaptchaToken, setHcaptchaToken] = useState('');
@@ -44,6 +45,11 @@ const RegisterPage = (props) => {
         const countries = countryList().getData();
         setCountries(countries);
     }, []);
+
+    useEffect(() => {
+        if(email && password && repeatPassword && userName && country && password === repeatPassword)
+            setSubmitable(true);
+    }, [inputs, captchaPassed]);
 
     const { email, password, repeatPassword, userName, country, refCode } = inputs;
     
@@ -72,8 +78,8 @@ const RegisterPage = (props) => {
             setCaptchaPassed(false);
             setHcaptchaToken('');
             captchaRef.current.resetCaptcha();
-        }
-        setConfirm(true);
+        } else setConfirm(true);
+
         if(email && password && country && userName && confirm && EmailValidator.validate(email) && captchaPassed) {
             const newUser = {
                 name: userName, 
@@ -196,7 +202,7 @@ const RegisterPage = (props) => {
                         </FormGroup>
                         <FormGroup className='actions d-flex justify-content-between m-0'>
                             <Link className='btn' to={Routes.Home.path}>Back</Link>
-                            <Button className='form-control bg-blue' onClick={handleRegister} disabled={!captchaPassed}>Next</Button>
+                            <Button className='form-control bg-blue' onClick={handleRegister} disabled={!submitable}>Next</Button>
                         </FormGroup>
                     </form>
                 </div>
